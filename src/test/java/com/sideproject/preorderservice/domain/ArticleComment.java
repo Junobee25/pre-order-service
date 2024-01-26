ce0bb8a9bec777efe3fbba4dbd7f2705bc200b7d
Junobee25 <wnsdhqo@naver.com>
2024-01-26 13:44:40 +0900

    #10 - 서비스에 필요한 도메인 코드 작성
    `AuditingFields`는 중복되는 메타 데이터를 추상메서드를 통해 상속받아 사용할 수 있도록 해당 클래스를 추가함.

---
+++ src/test/java/com/sideproject/preorderservice/domain/ArticleComment.java
@@ -0,0 +1,57 @@
+package com.sideproject.preorderservice.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@ToString(callSuper = true)
+@Entity
+public class ArticleComment extends AuditingFields{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
+    private Article article;
+
+    @Setter
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private UserAccount userAccount;
+
+    @Setter
+    @Column(updatable = false, length = 500)
+    private String content;
+
+    protected ArticleComment() {
+
+    }
+
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
+        this.article = article;
+        this.userAccount = userAccount;
+        this.content = content;
+    }
+
+    private static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleComment that = (ArticleComment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
