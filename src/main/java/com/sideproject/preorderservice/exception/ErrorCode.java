0217065203e232f2c03dba3fc17c6814759e67b0
Junobee25 <wnsdhqo@naver.com>
2024-01-28 15:57:35 +0900

    #8 - 예외 처리에 대한 클래스 및 Enum 생성
    예외 처리 메서드는 RuntimeException으로 일단 처리 했고, 메세지 같은 경우는 열거형으로 쓰면 될 것 같아 Enum을 통해서 정의했다.

--- src/main/java/com/sideproject/preorderservice/exception/ErrorCode.java
+++ src/main/java/com/sideproject/preorderservice/exception/ErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
+    EMAIL_AUTH_TOKEN_NOT_FOUNT(HttpStatus.NOT_FOUND, "Invalid email request"),
+    USER_NOT_AUTHENTICATED(HttpStatus.UNAUTHORIZED, "User not authenticated");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/sideproject/preorderservice/exception/PreOrderApplicationException.java
+++ src/main/java/com/sideproject/preorderservice/exception/PreOrderApplicationException.java
@@ -15,6 +15,11 @@ public class PreOrderApplicationException extends RuntimeException {
         this.message = null;
     }
 
+    //TODO: 원래 Enum으로 정의해서 예외 처리를 해주려 했는데 이메일 인증 예외 처리 과정에서 orElseThrow를 사용해야 되서 기본 생성자를 정의해서 사용함 리팩토링 필요
+    public PreOrderApplicationException() {
+
+    }
+
     @Override
     public String getMessage() {
         if (message == null) {
