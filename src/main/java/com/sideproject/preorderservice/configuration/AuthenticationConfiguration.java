a5416e1a6a3328da1157691f09989d32c7bfc623
Junobee25 <wnsdhqo@naver.com>
2024-01-29 13:38:09 +0900

    #14 - SpringSecurity Configuration 내부 빈 분리
    `UserAccountService`에서 비밀번호 암호화를 위해 BcrptPasswordEncoder를 사용중인데 시큐리티 설정 내부의 유저서비스 의존성 때문에 순환 참조가 발생함. 그래서 내부의 암호화를 위한 빈과 AuthenticationConfiguration을 분리하였다.

--- src/main/java/com/sideproject/preorderservice/configuration/AuthenticationConfiguration.java
+++ src/main/java/com/sideproject/preorderservice/configuration/AuthenticationConfiguration.java
@@ -9,14 +9,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-public class SecurityConfiguration {
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AuthenticationConfiguration {
+    private final UserAccountService userAccountService;
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
     @Bean
-    public BCryptPasswordEncoder encodePassword() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/api/join",
+                        "/api/login",
+                        "/join/confirm-email",
+                        "/api/follow"
+                ).permitAll()
+        ).addFilterBefore(new JwtTokenFilter(userAccountService, secretKey), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
