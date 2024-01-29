80783e5acaa94ecd18cd3570848d3f8eb09fb967
Junobee25 <wnsdhqo@naver.com>
2024-01-29 13:41:11 +0900

    #14 - 로그인 된 회원 인증을 위한 Jwt 토큰 유틸 , 필터 구현
    팔로우를 구현하기 전 로그인 된 회원정보를 가지고 상대방 회원을 팔로우하는 흐름이므로 인증에 대한 추가 Jwt 설정을 해주었다.
    `SpringSecurity Filter`를 사용하여 로그인 된 회원에 대한 인증을 구현했다.

---
+++ src/main/java/com/sideproject/preorderservice/configuration/filter/JwtTokenFilter.java
@@ -0,0 +1,65 @@
+package com.sideproject.preorderservice.configuration.filter;
+
+import com.sideproject.preorderservice.domain.UserAccount;
+import com.sideproject.preorderservice.dto.UserAccountDto;
+import com.sideproject.preorderservice.service.UserAccountService;
+import com.sideproject.preorderservice.util.JwtTokenUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final UserAccountService userAccountService;
+    private final String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // get header
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if(header == null || !header.startsWith("Bearer ")) {
+            log.error("Error occurs while getting header. header is null or invalid");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            final String token = header.split(" ")[1].trim();
+
+            if(JwtTokenUtils.isTokenExpired(token, secretKey)) {
+                log.error("key is expired");
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String email = JwtTokenUtils.getEmail(token, secretKey);
+            UserAccount userAccount = userAccountService.loadUserByEmail(email);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    //TODO
+                    userAccount, null, null
+            );
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (RuntimeException e) {
+            log.error("Error occurs while validation, {}", e.toString());
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
