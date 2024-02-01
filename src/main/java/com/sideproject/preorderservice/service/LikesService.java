8cf52b776783a53621e0011846fcc4f41d276058
Junobee25 <wnsdhqo@naver.com>
2024-02-01 21:49:15 +0900

    #29 - 좋아요 서비스 로직 메서드 분리
    하나의 좋아요 서비스 로직에 기존 좋아요 여부에 대한 로직을 같이 썻지만, 가독성이 매우 떨어졌다. 그래서 도메인 별로 메서드를 분리 하여 가독성을 높였다.
    
    add) Like에 대한 Response를 구현하려고 했지만 생각해보니 response정보를 어디에 사용할지 떠오르지 않았다. 굳이 클래스를 늘려서 복잡성을 높이고 싶지 않았기에 기존에 반환 타입인 Void로 유지하기로 결정하였다.

--- src/main/java/com/sideproject/preorderservice/service/LikesService.java
+++ src/main/java/com/sideproject/preorderservice/service/LikesService.java
@@ -28,41 +28,50 @@ public class LikesService {
                 .orElseThrow(() -> new PreOrderApplicationException(ErrorCode.USER_NOT_FOUND, String.format("user is %s", email)));
 
         Optional<Likes> optionalLikes = likesRepository.findByUserAccountAndLikeTypeAndTargetId(userAccount, likeType, targetId);
-
+        //TODO: 좋아요 타입이 더 많이 생길 경우 어떻게 처리 할 것인지 생각해 봐야 한다.
         if (likeType == LikeType.ARTICLE) {
             Article article = articleRepository.findById(targetId)
                     .orElseThrow(() -> new PreOrderApplicationException(ErrorCode.ARTICLE_NOT_FOUND, String.format("articleId is %d", targetId)));
-
-            if (optionalLikes.isPresent()) {
-                Likes likes = optionalLikes.get();
-                likes.toggleDeleted();
-                if (!likes.getDeleted()) {
-                    alarmEntityRepository.save(Alarm.of(article.getUserAccount(), userAccount.getId(), article.getId(), AlarmType.NEW_LIKE_ON_POST));
-                }
-                likesRepository.save(likes);
-            } else {
-                Likes newLikes = Likes.of(userAccount, likeType, targetId);
-                alarmEntityRepository.save(Alarm.of(article.getUserAccount(), userAccount.getId(), article.getId(), AlarmType.NEW_LIKE_ON_POST));
-                likesRepository.save(newLikes);
-            }
+            articleLikeToggle(article, targetId, optionalLikes, userAccount);
         }
 
         if (likeType == LikeType.COMMENT) {
             ArticleComment articleComment = articleCommentRepository.findById(targetId)
                     .orElseThrow(() -> new PreOrderApplicationException(ErrorCode.COMMENT_NOT_FOUND, String.format("commentId is %d", targetId)));
+            commentLikeToggle(articleComment, targetId, optionalLikes, userAccount);
+        }
+    }
 
-            if (optionalLikes.isPresent()) {
-                Likes likes = optionalLikes.get();
-                likes.toggleDeleted();
-                if (!likes.getDeleted()) {
-                    alarmEntityRepository.save(Alarm.of(articleComment.getUserAccount(), userAccount.getId(), articleComment.getId(), AlarmType.NEW_LIKE_ON_COMMENT));
-                }
-                likesRepository.save(likes);
-            } else {
-                Likes newLikes = Likes.of(userAccount, likeType, targetId);
-                alarmEntityRepository.save(Alarm.of(articleComment.getUserAccount(), userAccount.getId(), articleComment.getId(), AlarmType.NEW_LIKE_ON_COMMENT));
-                likesRepository.save(newLikes);
+    private void articleLikeToggle(Article article, Long targetId, Optional<Likes> optionalLikes, UserAccount userAccount) {
+        if (optionalLikes.isPresent()) {
+            Likes likes = optionalLikes.get();
+            likes.toggleDeleted();
+            if (!likes.getDeleted()) {
+                alarmEntityRepository.save(Alarm.of(article.getUserAccount(), userAccount.getId(), article.getId(), AlarmType.NEW_LIKE_ON_POST));
             }
+            likesRepository.save(likes);
+        }
+        if (optionalLikes.isEmpty()) {
+            Likes newLikes = Likes.of(userAccount, LikeType.ARTICLE, targetId);
+            alarmEntityRepository.save(Alarm.of(article.getUserAccount(), userAccount.getId(), article.getId(), AlarmType.NEW_LIKE_ON_POST));
+            likesRepository.save(newLikes);
+        }
+    }
+
+    private void commentLikeToggle(ArticleComment articleComment, Long targetId, Optional<Likes> optionalLikes, UserAccount userAccount) {
+        if (optionalLikes.isPresent()) {
+            Likes likes = optionalLikes.get();
+            likes.toggleDeleted();
+            if (!likes.getDeleted()) {
+                alarmEntityRepository.save(Alarm.of(articleComment.getUserAccount(), userAccount.getId(), articleComment.getId(), AlarmType.NEW_LIKE_ON_COMMENT));
+            }
+            likesRepository.save(likes);
+        }
+        if (optionalLikes.isEmpty()) {
+            Likes newLikes = Likes.of(userAccount, LikeType.COMMENT, targetId);
+            alarmEntityRepository.save(Alarm.of(articleComment.getUserAccount(), userAccount.getId(), articleComment.getId(), AlarmType.NEW_LIKE_ON_COMMENT));
+            likesRepository.save(newLikes);
         }
     }
 }
+
