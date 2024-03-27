4e30089d06909b8e99c6492799675383e80e1aa5
Junobee25 <wnsdhqo@naver.com>
2024-03-27 14:01:21 +0900

    test - controller

--- src/test/java/com/sideproject/preorderservice/controller/UserAccountControllerTest.java
+++ src/test/java/com/sideproject/preorderservice/controller/UserAccountControllerTest.java
@@ -15,9 +15,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,11 +35,22 @@ class UserAccountControllerTest {
     @DisplayName("[POST] 회원 가입 - 정상 호출")
     @Test
     void join() throws Exception {
+        // given
         UserAccountDto userAccountDto = UserAccountDto.of(1L, "test", "test", "test", true, "test");
         given(userAccountService.join(any(), any(), any(), any()))
                 .willReturn(userAccountDto);
         // when && then
         mvc.perform(post("/api/join").with(csrf()))
+                .andExpect(status().isOk());
+
+    }
+
+    @DisplayName("[POST] 유효한 이메일 인증 - 정상 호출")
+    @Test
+    void confirmEmail() throws Exception {
+        willDoNothing().given(userAccountService).confirmEmail("test","test");
+
+        mvc.perform(get("/join/confirm-email").with(csrf()))
                 .andExpect(status().isOk());
 
     }
