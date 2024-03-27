f724872d164f036d719738192ee77399f0f25bcd
Junobee25 <wnsdhqo@naver.com>
2024-03-27 23:34:27 +0900

    refactor..

--- src/main/java/com/sideproject/preorderservice/domain/entity/UserAccount.java
+++ src/main/java/com/sideproject/preorderservice/domain/entity/UserAccount.java
@@ -1,6 +1,7 @@
 package com.sideproject.preorderservice.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -21,6 +22,7 @@ import java.util.Objects;
 })
 
 @Entity
+@Data
 public class UserAccount implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
